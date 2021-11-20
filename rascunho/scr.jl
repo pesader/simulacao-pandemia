8fcdb3f2931e2d057df96bfbec54e9d12426f2fd
Pedro Sader Azevedo <pedro.saderazevedo@gmail.com>
2021-11-20 14:59:52 -0300

    iniciar experimentos com Pluto notebooks

---
+++ rascunho/scr.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.17.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ aa87eab8-48c1-11ec-006a-7d0f37bed83e
+md"""
+this is a cell full of markdown. It would be nice if didn't interfere with the code, right? I think so!
+"""
+
+# ╔═╡ ecf1265e-951f-47a0-bbec-d0caf20786ac
+begin
+	a = [1:1:10;]
+	b = [10:-1:0;]
+	@show(a)
+	@show(b)
+end
+
+# ╔═╡ b6c29194-85ab-4f65-9759-9bfabb14bcb1
+md"""
+wait, more markdown? Are you serious?
+Yeah I wanna test it out and see how well it works.
+"""
+
+# ╔═╡ Cell order:
+# ╟─aa87eab8-48c1-11ec-006a-7d0f37bed83e
+# ╠═ecf1265e-951f-47a0-bbec-d0caf20786ac
+# ╟─b6c29194-85ab-4f65-9759-9bfabb14bcb1
